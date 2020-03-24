a50ebd2204635e7f33720dbbba96219e3f82db92
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-24 01:38:54 -0300

    Configuring the Lobby Interface

---
+++ Nightmare-Photon/Assets/MyGame/Scripts/Lobby.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lobby : MonoBehaviour {
+
+    public GameObject panelLogin;
+    public GameObject panelLobby;
+
+    public Text lobbyWait;
+    public Text lobbyTimeStart;
+
+    public string lobbyTimeStartText = "Start Game in {0}...";
+
+	public void Start ()
+    {
+        lobbyTimeStart.gameObject.SetActive(false);
+        PanelLoginActive();
+	}
+
+    public void PanelLobbyActive()
+    {
+        panelLogin.SetActive(false);
+        panelLobby.SetActive(true);
+    }
+
+    public void PanelLoginActive()
+    {
+        panelLogin.SetActive(true);
+        panelLobby.SetActive(false);
+    }
+}
---
+++ Nightmare-Photon/Assets/MyGame/Scripts/NetworkController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkController : MonoBehaviourPunCallbacks {
+
+    public Lobby lobbyScript;
+
+    public override void OnConnected()
+    {
+        Debug.Log("OnConnected");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster");
+
+        lobbyScript.PanelLobbyActive();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("OnDisconnected: " + cause.ToString());
+
+        lobbyScript.PanelLoginActive();
+    }
+
+    public void CancelButton()
+    {
+        PhotonNetwork.Disconnect(); // print: DisconnectByClientLogic
+    }
+
+    public void LoginButton()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+}
