533eb0b81d283f2ee5fc659499d2226defb8b635
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-24 22:06:08 -0300

    Setting Player Name

---
+++ Nightmare-Photon/Assets/MyGame/Scripts/NameMultiplayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class NameMultiplayer : MonoBehaviour {
+
+    PhotonView photonView;
+
+    public Text playerNameText;
+    public GameObject playerCanvas;
+	
+	void Start () {
+        photonView = GetComponent<PhotonView>();
+        playerNameText.text = photonView.Owner.NickName;
+	}
+
+    private void Update()
+    {
+        playerCanvas.transform.LookAt(Camera.main.transform);
+    }
+}
