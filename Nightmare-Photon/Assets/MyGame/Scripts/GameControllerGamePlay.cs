496dd9f9073ecc9e0b4e9ea78984cdced8cf9e75
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-24 21:28:06 -0300

    Spawn Player configuration

---
+++ Nightmare-Photon/Assets/MyGame/Scripts/GameControllerGamePlay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GameControllerGamePlay : MonoBehaviourPunCallbacks {
+
+    public GameObject myPlayer;
+    public Transform[] spawnPLayer;
+
+    private void Start()
+    {
+        int position = Random.Range(0, spawnPLayer.Length);
+        PhotonNetwork.Instantiate(myPlayer.name, spawnPLayer[position].position, spawnPLayer[position].rotation, 0);
+    }
+}
